cd1b99745f5da67beaa611abe7d7ee9e13f7ee7a
n0bisuke <rsksound@gmail.com>
2022-08-31 15:04:05 +0900

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const express = require("express");
+const RED = require("node-red");
+
+// Expressアプリケーションの生成
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// 静的コンテンツのルートを追加
+app.use("/",express.static("public"));
+
+// サーバの生成
+var server = http.createServer(app);
+
+// 設定オブジェクトの生成 - 他のオプションについてはデフォルトの 'settings.js' ファイルを参照してください
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"/home/nol/.nodered/",
+    functionGlobalContext: { }    // グローバルコンテキストを有効化
+};
+
+// サーバと設定とランタイムの初期化
+RED.init(server,settings);
+
+// エディタUIのルートを '/red' に指定
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// HTTP node UIのルートを '/api' に指定
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(PORT);
+
+// ランタイム起動
+RED.start();
